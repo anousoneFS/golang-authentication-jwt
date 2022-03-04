365e5f73ed7512d29ad8150f1ede14f4f43c7b84
Anousone-FS <anousone.litd@gmail.com>
2022-03-05 06:09:34 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("hello sone")
+	})
+	app.Listen(":8000")
+}
