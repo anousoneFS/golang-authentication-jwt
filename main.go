2078038cfd52d5ebba67fb0ea1d08d54f922f507
Anousone-FS <anousone.litd@gmail.com>
2022-03-05 06:34:15 +0700

    connect to mysql

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
+	_, err := gorm.Open(mysql.Open("root:admin123@tcp(127.0.0.1:3306)/test"), &gorm.Config{})
+	if err != nil {
+		panic("cloud not connect to the database")
+	}
 	app := fiber.New()
 	app.Get("/hello", func(c *fiber.Ctx) error {
 		return c.SendString("hello sone")
