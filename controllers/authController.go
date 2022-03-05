51611d3a29fab654c19daf7df88c5d0e219b26d2
Anousone-FS <anousone.litd@gmail.com>
2022-03-05 07:28:49 +0700

    login

--- controllers/authController.go
+++ controllers/authController.go
@@ -22,3 +22,26 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 	return c.JSON(user)
 }
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	var user models.User
+	// 1: check user email in database
+	database.DB.Where("email = ?", data["email"]).First(&user)
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{"message": "user not found"})
+	}
+	// 2: check password
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+	// 3: return result
+	return c.JSON(user)
+}
