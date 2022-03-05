080b0ebad08fba2afebb014d3d454328583325fb
Anousone-FS <anousone.litd@gmail.com>
2022-03-05 07:07:14 +0700

    hash password

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,6 +1,10 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/anousoneFS/golang-jwt/models"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -8,5 +12,11 @@ func Register(c *fiber.Ctx) error {
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
-	return c.JSON(data)
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+	return c.JSON(user)
 }
--- databases/connection.go
+++ databases/connection.go
@@ -1,13 +1,18 @@
 package database
 
 import (
+	"github.com/anousoneFS/golang-jwt/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
-	_, err := gorm.Open(mysql.Open("root:admin123@tcp(127.0.0.1:3306)/test"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open("root:admin123@tcp(127.0.0.1:3306)/test"), &gorm.Config{})
 	if err != nil {
 		panic("cloud not connect to the database")
 	}
+	DB = connection
+	connection.AutoMigrate(&models.User{})
 }
