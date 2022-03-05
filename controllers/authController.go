e8a11d155cfaa9aeeded3ffecac07f819cec353e
Anousone-FS <anousone.litd@gmail.com>
2022-03-05 09:32:02 +0700

    logout

--- controllers/authController.go
+++ controllers/authController.go
@@ -49,11 +49,12 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
+	// 3: create claims
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
 		Issuer:    strconv.Itoa(int(user.Id)),
 		ExpiresAt: &jwt.Time{jwt.Now().Add(time.Hour)},
 	})
-
+	// 4: create token
 	token, err := claims.SignedString([]byte(SecretKey))
 	if err != nil {
 		c.Status(fiber.StatusInternalServerError)
@@ -90,4 +91,17 @@ func User(c *fiber.Ctx) error {
 	var user models.User
 	database.DB.Where("id = ?", claims.Issuer).First(&user)
 	return c.JSON(user)
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
